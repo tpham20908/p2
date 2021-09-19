96317669d64779485f849a4371f9c2ac29ae42f7
Tung Pham <tpham20908@gmail.com>
2021-09-19 10:10:13 -0400

    ico icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,44 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+	Container,
+	Div1,
+	Div2,
+	Div3,
+	NavLink,
+	SocialIcons,
+} from './HeaderStyles';
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+	<Container>
+		<Div1>
+			<Link href='/'>
+				<a style={{ display: 'flex', alignItems: 'center', color: '#fff' }}>
+					<DiCssdeck />
+					<span style={{ marginLeft: 4 }}>Portfolio</span>
+				</a>
+			</Link>
+		</Div1>
+
+		<Div2>
+			<li>
+				<Link href='#projects'>
+					<NavLink>Projects</NavLink>
+				</Link>
+			</li>
+			<li>
+				<Link href='#tech'>
+					<NavLink>Technologies</NavLink>
+				</Link>
+			</li>
+			<li>
+				<Link href='#about'>
+					<NavLink>About</NavLink>
+				</Link>
+			</li>
+		</Div2>
+	</Container>
 );
 
 export default Header;
