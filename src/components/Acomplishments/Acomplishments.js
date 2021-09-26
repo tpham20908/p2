4714483ac27826d1c6eb0265a74d2cf2ff3579f2
Tung Pham <tpham20908@gmail.com>
2021-09-26 17:59:46 -0400

    done

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,19 +1,36 @@
 import React from 'react';
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import {
+	Section,
+	SectionDivider,
+	SectionTitle,
+} from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+	{ number: 20, text: 'Open Source Projects' },
+	{ number: 1000, text: 'Students' },
+	{ number: 1900, text: 'Github Followers' },
+	{ number: 5000, text: 'Github Stars' },
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+	<Section>
+		<SectionTitle>Personal Accoplishment</SectionTitle>
+		<SectionDivider />
+		<Boxes>
+			{data.map((card, idx) => {
+				const { number, text } = card;
+				return (
+					<Box key={idx}>
+						<BoxNum>{number}</BoxNum>
+						<BoxText>{text}</BoxText>
+					</Box>
+				);
+			})}
+		</Boxes>
+		<br />
+	</Section>
 );
 
 export default Acomplishments;
--- src/components/TimeLine/TimeLineStyles.js
+++ src/components/TimeLine/TimeLineStyles.js
@@ -70,7 +70,7 @@ export const CarouselItemTitleContainer = styled.div`
 `;
 
 export const CarouselItemTitle = styled.h4`
-	font-weight: bold;
+	font-weight: 300;
 	font-size: 24px;
 	line-height: 32px;
 	letter-spacing: 0.02em;
