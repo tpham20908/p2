96e0d19021594dd42bd3eecba2a5429007148cf8
Tung Pham <tpham20908@gmail.com>
2023-06-18 10:11:38 -0400

    update timeline

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,8 @@
 export const projects = [
 	{
 		title: 'MERN Memories',
-		description: 'This is a simple social media app.',
+		description:
+			'A social media app that allows users to share and store their favorite memories with others. Built using the MERN stack (MongoDB, Express, React, Node).',
 		image: '/images/1.png',
 		tags: ['Mongo', 'Express', 'React', 'Node'],
 		source: 'https://github.com/tpham20908/memories_server',
@@ -12,7 +13,7 @@ export const projects = [
 	{
 		title: 'TP - Travel Advisor',
 		description:
-			"A gift for anyone who visites my website. You can search for Restaurants, Hotels, Atrractions near you or anywhere in the world. It's a must have app when on vacation.",
+			'Discover the best restaurants, hotels, and attractions around the world with TP - Travel Advisor. Find personalized recommendations and plan your perfect vacation. Built with React and integrated with RapidAPI for comprehensive data.',
 		image: '/images/travel_advisor.png',
 		tags: ['React', 'RapidAPI'],
 		source: 'https://github.com/tpham20908/travel_advisor',
@@ -21,7 +22,8 @@ export const projects = [
 	},
 	{
 		title: 'Phone store E-Commerce',
-		description: 'Phone store (E-commerce) application.',
+		description:
+			'Experience a seamless shopping experience for the latest phones with the Phone Store E-Commerce application. Browse and purchase phones online with ease. Developed using React and JavaScript.',
 		image: '/images/2.png',
 		tags: ['React', 'JavaScript'],
 		source: 'https://github.com/tpham20908/react-phone-store',
@@ -30,7 +32,8 @@ export const projects = [
 	},
 	{
 		title: 'COVID-19 tracker App',
-		description: 'Application to track COVID-19 cases.',
+		description:
+			'Stay informed about COVID-19 cases worldwide with the COVID-19 Tracker application. Track real-time statistics and visualize the spread of the virus. Developed using React and RESTful APIs.',
 		image: '/images/covid-19-tracker.png',
 		tags: ['React', 'RESTful'],
 		source: 'https://github.com/tpham20908/covid19-tracker',
@@ -39,7 +42,8 @@ export const projects = [
 	},
 	{
 		title: 'MERN with CRUD',
-		description: 'MERM stack with fully CRUD functionality',
+		description:
+			'Experience a fully functional CRUD application with the MERN stack (MongoDB, Express, React, Node). Create, read, update, and delete events seamlessly. Perfect for managing planned events. ',
 		image: '/images/mern-crud.jpeg',
 		tags: ['Node', 'Express', 'MongoDB', 'React'],
 		source: 'https://github.com/tpham20908/planned-events',
@@ -48,7 +52,8 @@ export const projects = [
 	},
 	{
 		title: 'Unichat',
-		description: 'Full Realtime Chat Application',
+		description:
+			'Engage in real-time conversations with Unichat, a full-featured chat application. Connect with friends and enjoy seamless communication. Built with React, ChatEngine, and Firebase.',
 		image: '/images/4.jpg',
 		tags: ['React', 'ChatEngine', 'Firebase'],
 		source: 'https://google.com',
@@ -60,18 +65,22 @@ export const projects = [
 export const TimeLineData = [
 	{
 		year: 2017,
-		text: 'Desire to be Software Developer sent me back to College',
+		text: 'Enrolled in college to pursue a career as a Software Developer',
 	},
 	{
 		year: 2019,
-		text: 'Spent my internship period at Kaayu.ca as a Full-stack Developer',
+		text: 'Completed a valuable internship as a Full-stack Developer at Kaayu.ca',
 	},
 	{
 		year: 2019,
-		text: 'Finished school and worked as a Javascropt/Full-stack Developer at Hesper.io',
+		text: 'Graduated from college and joined Hesper.io as a JavaScript/Full-stack Developer',
 	},
 	{
 		year: 2020,
-		text: 'Work for Dynamicly as a Application/Full-stack Developer',
+		text: 'Joined Dynamicly as an Application/Full-stack Developer, contributing to impactful projects',
+	},
+	{
+		year: 2023,
+		text: 'Currently employed as a Software Developer at McGill University, contributing to IT applications portfolio',
 	},
 ];
